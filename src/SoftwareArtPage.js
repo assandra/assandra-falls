85d74b2df8b67b3619059d28975e4845675c5256
assandra.falls <assandra.falls@stream-technologies.com>
2020-11-24 18:18:25 +0000

    Add all pages

--- src/SoftwareArtPage.js
+++ src/SoftwareArtPage.js
@@ -2,7 +2,106 @@ import React, { Component } from "react";
 
 class SoftwareArtPage extends Component {
   render() {
-    return <h1>Software Art</h1>;
+    return(
+      <div className="content">
+      <h1>Software Art</h1>
+      <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          viverra nibh ligula, nec viverra sem commodo non. Donec imperdiet
+          semper lobortis. Aliquam erat volutpat. Nullam sed nunc vel ex commodo
+          fermentum in consectetur mauris. Fusce sagittis eu metus ut consequat.
+          Fusce ut massa orci. Nunc eu magna eu odio accumsan commodo.
+          Suspendisse quis velit leo.
+        </p>
+        <p>
+          Fusce accumsan ultrices ligula, sit amet consequat nibh eleifend ut.
+          Proin quis blandit mauris. Maecenas bibendum dapibus dignissim. Nulla
+          consequat velit luctus, hendrerit turpis et, condimentum massa.
+          Pellentesque ut dui nec ex suscipit luctus. Nulla ornare maximus
+          sapien, at aliquet lectus fermentum sagittis. Proin ac volutpat ante,
+          at ultrices ipsum. Mauris elementum pulvinar velit quis commodo.
+          Aenean nisi augue, lobortis id tincidunt ut, bibendum id eros. Nam ac
+          ligula non lectus viverra suscipit. Integer elementum turpis mattis
+          porttitor commodo. Nulla egestas commodo mi sit amet cursus.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          viverra nibh ligula, nec viverra sem commodo non. Donec imperdiet
+          semper lobortis. Aliquam erat volutpat. Nullam sed nunc vel ex commodo
+          fermentum in consectetur mauris. Fusce sagittis eu metus ut consequat.
+          Fusce ut massa orci. Nunc eu magna eu odio accumsan commodo.
+          Suspendisse quis velit leo.
+        </p>
+        <p>
+          Suspendisse potenti. Vestibulum bibendum rhoncus metus, vitae
+          vulputate metus gravida nec. Praesent dignissim mauris at tortor
+          posuere, eu ullamcorper justo ullamcorper. Nunc eget lectus libero. In
+          sem dui, varius nec tincidunt id, tempor ut lacus. Praesent maximus
+          nisl dolor, eu condimentum diam aliquam vitae. Aenean rhoncus
+          fermentum dui, id ullamcorper tellus tempor vitae. Praesent finibus mi
+          vitae sollicitudin pellentesque. Morbi rutrum ligula quam, sed
+          porttitor tortor iaculis vel. Pellentesque tempus laoreet enim.
+        </p>
+        <p>
+          Fusce accumsan ultrices ligula, sit amet consequat nibh eleifend ut.
+          Proin quis blandit mauris. Maecenas bibendum dapibus dignissim. Nulla
+          consequat velit luctus, hendrerit turpis et, condimentum massa.
+          Pellentesque ut dui nec ex suscipit luctus. Nulla ornare maximus
+          sapien, at aliquet lectus fermentum sagittis. Proin ac volutpat ante,
+          at ultrices ipsum. Mauris elementum pulvinar velit quis commodo.
+          Aenean nisi augue, lobortis id tincidunt ut, bibendum id eros. Nam ac
+          ligula non lectus viverra suscipit. Integer elementum turpis mattis
+          porttitor commodo. Nulla egestas commodo mi sit amet cursus.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          viverra nibh ligula, nec viverra sem commodo non. Donec imperdiet
+          semper lobortis. Aliquam erat volutpat. Nullam sed nunc vel ex commodo
+          fermentum in consectetur mauris. Fusce sagittis eu metus ut consequat.
+          Fusce ut massa orci. Nunc eu magna eu odio accumsan commodo.
+          Suspendisse quis velit leo.
+        </p>
+        <p>
+          Suspendisse potenti. Vestibulum bibendum rhoncus metus, vitae
+          vulputate metus gravida nec. Praesent dignissim mauris at tortor
+          posuere, eu ullamcorper justo ullamcorper. Nunc eget lectus libero. In
+          sem dui, varius nec tincidunt id, tempor ut lacus. Praesent maximus
+          nisl dolor, eu condimentum diam aliquam vitae. Aenean rhoncus
+          fermentum dui, id ullamcorper tellus tempor vitae. Praesent finibus mi
+          vitae sollicitudin pellentesque. Morbi rutrum ligula quam, sed
+          porttitor tortor iaculis vel. Pellentesque tempus laoreet enim.
+        </p>
+        <p>
+          Fusce accumsan ultrices ligula, sit amet consequat nibh eleifend ut.
+          Proin quis blandit mauris. Maecenas bibendum dapibus dignissim. Nulla
+          consequat velit luctus, hendrerit turpis et, condimentum massa.
+          Pellentesque ut dui nec ex suscipit luctus. Nulla ornare maximus
+          sapien, at aliquet lectus fermentum sagittis. Proin ac volutpat ante,
+          at ultrices ipsum. Mauris elementum pulvinar velit quis commodo.
+          Aenean nisi augue, lobortis id tincidunt ut, bibendum id eros. Nam ac
+          ligula non lectus viverra suscipit. Integer elementum turpis mattis
+          porttitor commodo. Nulla egestas commodo mi sit amet cursus.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          viverra nibh ligula, nec viverra sem commodo non. Donec imperdiet
+          semper lobortis. Aliquam erat volutpat. Nullam sed nunc vel ex commodo
+          fermentum in consectetur mauris. Fusce sagittis eu metus ut consequat.
+          Fusce ut massa orci. Nunc eu magna eu odio accumsan commodo.
+          Suspendisse quis velit leo.
+        </p>
+        <p>
+          Suspendisse potenti. Vestibulum bibendum rhoncus metus, vitae
+          vulputate metus gravida nec. Praesent dignissim mauris at tortor
+          posuere, eu ullamcorper justo ullamcorper. Nunc eget lectus libero. In
+          sem dui, varius nec tincidunt id, tempor ut lacus. Praesent maximus
+          nisl dolor, eu condimentum diam aliquam vitae. Aenean rhoncus
+          fermentum dui, id ullamcorper tellus tempor vitae. Praesent finibus mi
+          vitae sollicitudin pellentesque. Morbi rutrum ligula quam, sed
+          porttitor tortor iaculis vel. Pellentesque tempus laoreet enim.
+        </p>
+      </div>
+    ) 
   }
 }
 
