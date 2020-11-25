b41ef08771982a123a73778370cbebdd8600c599
assandra.falls <assandra.falls@stream-technologies.com>
2020-11-25 12:45:38 +0000

    Add ContactUsForm component

---
+++ src/ContactUsForm.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+
+class ContactUsForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+    };
+  }
+
+  handleChange = (event) => {
+    const value = event.target.value;
+    this.setState({ ...this.state, [event.target.id]: value });
+  };
+
+  handleSubmit = (event) => {
+    alert("Info submitted was: " + '\n' + this.state.name + '\n' + this.state.email + '\n' + this.state.message);
+    event.preventDefault();
+  };
+
+  isValid = () => {
+    return this.state.name === "" ||
+      this.state.email === "" ||
+      this.state.message === ""
+      ? false
+      : true;
+  };
+
+  render() {
+    return (
+      <div className="form" >
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          id="name"
+          placeholder="Your Name"
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          id="email"
+          placeholder="E-mail"
+          value={this.state.email}
+          onChange={this.handleChange}
+        />
+        <br />
+        <br />
+        <textarea
+          type="text"
+          id="message"
+          placeholder="Enter your message"
+          value={this.state.message}
+          onChange={this.handleChange}
+        />
+        <br />
+        <br />
+        <button disabled={!this.isValid()} type="submit">
+          Submit
+        </button>
+      </form>
+      </div>
+    );
+  }
+}
+
+export default ContactUsForm;
